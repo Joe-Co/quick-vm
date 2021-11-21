51faa964226e218e8b864f75929158663cbe3c35
Joe-Co <tusjoe@gmail.com>
2021-11-21 16:57:13 -0700

    project created with cargo

---
+++ v0/target/rls/debug/deps/v0-ddad91782ed7d83b.d
@@ -0,0 +1,5 @@
+/Users/josephcohen/Desktop/Pierce VM/quick-vm/v0/target/rls/debug/deps/v0-ddad91782ed7d83b.rmeta: src/main.rs
+
+/Users/josephcohen/Desktop/Pierce VM/quick-vm/v0/target/rls/debug/deps/v0-ddad91782ed7d83b.d: src/main.rs
+
+src/main.rs:
